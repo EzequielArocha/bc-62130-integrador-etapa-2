27e77e997d1c33cee28a0dbb807f534385a27653
Ezequiel Arocha <ezequiel_gonzalo@hotmail.com>
2023-10-16 19:37:27 -0300

    Agregamos tabla

--- src/components/Tabla.jsx
+++ src/components/Tabla.jsx
@@ -1,7 +1,33 @@
+import { useContext } from "react";
 import "./Tabla.scss";
+import TablaFila from "./TablaFila";
+import ProductoContext from "../contexts/ProductoContext";
 
 const Tabla = () => {
-  return <div>Tabla</div>;
+  const { productos } = useContext(ProductoContext);
+  return (
+    <table className="tabla-alta">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Precio</th>
+          <th>Stock</th>
+          <th>Marca</th>
+          <th>Categoría</th>
+          <th>Detalles</th>
+          <th>Foto</th>
+          <th>Envío</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {productos &&
+          productos.map((producto, idx) => (
+            <TablaFila key={idx} producto={producto} />
+          ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Tabla;
--- src/components/TablaFila.jsx
+++ src/components/TablaFila.jsx
@@ -1,7 +1,23 @@
-import "./tablaFila.scss";
+import "./TablaFila.scss";
 
-const TablaFila = () => {
-  return <div>TablaFila</div>;
+const TablaFila = ({ producto }) => {
+  return (
+    <tr>
+      <td>{producto.nombre}</td>
+      <td>{producto.precio}</td>
+      <td>{producto.stock}</td>
+      <td>{producto.categoria}</td>
+      <td>{producto.detalles}</td>
+      <td>
+        <img id="img-row" src={producto.foto} alt={producto.nombre} />
+      </td>
+      <td>{producto.envio ? "Si" : "No"}</td>
+      <td>
+        <button onClick={() => handleUpdate(producto)}>Editar</button>
+        <button onClick={() => handleDelete(producto.id)}>Eliminar</button>
+      </td>
+    </tr>
+  );
 };
 
 export default TablaFila;
