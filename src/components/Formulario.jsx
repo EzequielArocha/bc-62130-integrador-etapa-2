af5e87028f209351c9fdad543cf45c569177bb57
Ezequiel Arocha <ezequiel_gonzalo@hotmail.com>
2023-10-16 19:07:34 -0300

    Agrego el componente formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const Formulario = () => {
-  return <div>Formulario</div>;
+  return (
+    <>
+      <h3>Agregar : Editar</h3>;
+      <form>
+        <div>
+          <label htmlFor="lbl-nombre">Nombre</label>
+          <input type="text" name="nombre" id="lbl-nombre" />
+        </div>
+        <div>
+          <label htmlFor="lbl-precio">Precio</label>
+          <input type="text" name="precio" id="lbl-precio" />
+        </div>
+        <div>
+          <label htmlFor="lbl-stock">Stock</label>
+          <input type="text" name="stock" id="lbl-stock" />
+        </div>
+        <div>
+          <label htmlFor="lbl-marca">Marca</label>
+          <input type="text" name="marca" id="lbl-marca" />
+        </div>
+        <div>
+          <label htmlFor="lbl-detalles">Detalles</label>
+          <input type="text" name="detalles" id="lbl-detalles" />
+        </div>
+        <div>
+          <label htmlFor="lbl-foto">Foto</label>
+          <input type="text" name="foto" id="lbl-foto" />
+        </div>
+        <div>
+          <label htmlFor="lbl-envio">Envío</label>
+          <input type="checkbox" name="envio" id="lbl-envio" />
+        </div>
+        <button type="submit">Guardar</button>
+        <button type="reset">Limpiar</button>
+      </form>
+    </>
+  );
 };
 
 export default Formulario;
--- src/pages/Alta.jsx
+++ src/pages/Alta.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import Tabla from "../components/Tabla";
+import Formulario from "../components/Formulario";
+
 const Alta = () => {
-  return <div>Alta</div>;
+  return (
+    <>
+      <h1>Formulario de alta de productos</h1>
+      <h2>Agregar productos</h2>
+      <Formulario />
+      <hr />
+      <Tabla />
+    </>
+  );
 };
 
 export default Alta;
