bebacd7b144a86c2c40a58e40598699c2e5ca67c
Ezequiel Arocha <ezequiel_gonzalo@hotmail.com>
2023-10-17 22:01:19 -0300

    Empiezo a laburar con el formulario y el agregar productos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,37 +1,100 @@
-import React from "react";
+import { useState } from "react";
+
+const formInicial = {
+  id: null,
+  Nombre: "",
+  Precio: "",
+  Stock: "",
+  Marca: "",
+  Detalles: "",
+  Foto: "",
+  Envío: false,
+};
 
 const Formulario = () => {
+  const [form, setForm] = useState(formInicial);
+  const handleChange = (e) => {
+    const { type, name, checked, value } = e.target;
+    setForm({
+      ...form,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+  const handleSubmit = () => {};
+  const handleReset = () => {};
   return (
     <>
       <h3>Agregar : Editar</h3>;
       <form>
         <div>
           <label htmlFor="lbl-nombre">Nombre</label>
-          <input type="text" name="nombre" id="lbl-nombre" />
+          <input
+            type="text"
+            name="nombre"
+            id="lbl-nombre"
+            value={form.nombre}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-precio">Precio</label>
-          <input type="text" name="precio" id="lbl-precio" />
+          <input
+            type="text"
+            name="precio"
+            id="lbl-precio"
+            value={form.Precio}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-stock">Stock</label>
-          <input type="text" name="stock" id="lbl-stock" />
+          <input
+            type="text"
+            name="stock"
+            id="lbl-stock"
+            value={form.Stock}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-marca">Marca</label>
-          <input type="text" name="marca" id="lbl-marca" />
+          <input
+            type="text"
+            name="marca"
+            id="lbl-marca"
+            value={form.Marca}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-detalles">Detalles</label>
-          <input type="text" name="detalles" id="lbl-detalles" />
+          <input
+            type="text"
+            name="detalles"
+            id="lbl-detalles"
+            value={form.Detalles}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-foto">Foto</label>
-          <input type="text" name="foto" id="lbl-foto" />
+          <input
+            type="text"
+            name="foto"
+            id="lbl-foto"
+            value={form.Foto}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="lbl-envio">Envío</label>
-          <input type="checkbox" name="envio" id="lbl-envio" />
+          <input
+            type="checkbox"
+            name="envio"
+            id="lbl-envio"
+            checked={form.Envío}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">Guardar</button>
         <button type="reset">Limpiar</button>
